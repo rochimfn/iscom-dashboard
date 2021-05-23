79940fc5eecae77a160403d7bdf150a6424d38c7
Rochim Farul Noviyan <rochim.noviyan@gmail.com>
2021-05-23 12:17:53 +0700

    DB: Change admin default username
    Username default sebelumnya dinilai terlalu alay sehingga pada commit
    ini dilakukan perubahan.

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -15,14 +15,14 @@ class UserSeeder extends Seeder
     public function run()
     {
         $user = [
-        	[
-        		'user_name' => 'adminnich',
-        		'email' => 'rochim@localhost',
-        		'user_role_id' => 1,
-        		'password' => Hash::make('password'),
+            [
+                'user_name' => 'administrator',
+                'email' => 'rochim@localhost',
+                'user_role_id' => 1,
+                'password' => Hash::make('password'),
                 'created_at' => Carbon::now(),
                 'updated_at' => Carbon::now()
-        	]
+            ]
         ];
 
         DB::table('users')->insert($user);
