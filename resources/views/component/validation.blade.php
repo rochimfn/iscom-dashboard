f5c7518b53c44c4a2ca00d6cc2f7393b13618fdd
Rochim Farul Noviyan <rochim.noviyan@gmail.com>
2021-05-23 12:07:11 +0700

    View: Adding support for multiple success message
    Sebelumnya pesan sukses hanya mendukung string tunggal. Dengan adanya
    commit ini pesan sukses dapat didefinisikan dalam bentuk array of
    string. Nantinya pesan tersebut akan ditampilkan dalam bentuk daftar.

--- resources/views/component/validation.blade.php
+++ resources/views/component/validation.blade.php
@@ -1,14 +1,22 @@
 @if ($errors->any())
-    <div class="alert bg-danger mt-1">
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
+<div class="alert bg-danger mt-1">
+    <ul>
+        @foreach ($errors->all() as $error)
+        <li>{{ $error }}</li>
+        @endforeach
+    </ul>
+</div>
 @endif
 @if (session('success'))
-    <div class="alert bg-lime mt-1">
-        <p>{{ session('success') }}</p>
-    </div>
+<div class="alert bg-lime mt-1">
+    @if(is_array(session('success')))
+    <ul>
+        @foreach (session('success') as $msg)
+        <li>{{ $msg }}</li>
+        @endforeach
+    </ul>
+    @elseif (is_string(session('success')))
+    <p>{{ session('success') }}</p>
+    @endif
+</div>
 @endif
