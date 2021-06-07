9809fd72af09d754f5abfd6669e298ac0c86a8c0
Rochim Farul Noviyan <rochim.noviyan@gmail.com>
2021-06-07 12:57:10 +0700

    Config: Disable reset password

--- config/adminlte.php
+++ config/adminlte.php
@@ -203,9 +203,9 @@ return [
 
     'register_url' => 'register',
 
-    'password_reset_url' => 'password/reset',
+    'password_reset_url' => null,
 
-    'password_email_url' => 'password/email',
+    'password_email_url' => null,
 
     'profile_url' => false,
 
